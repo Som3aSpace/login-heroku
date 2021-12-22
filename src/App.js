6988a51a883db13caeb97ce1571d760fb648eeb6
ibrahimi <mohamed.ibrahim.ext@dematic.com>
2021-12-22 12:46:15 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Grid,Form,Segment,Button,Divider,Image } from 'semantic-ui-react'
+
+import './index.css'
+
+export default class App extends Component {
+    state = {
+        firstName : '',
+        lastName : '',
+        emailId: "constant@mail.com"
+
+    }
+
+    handleInput = (name) => (e) =>{
+        this.setState({[name] : e.target.value})
+    }
+    submit = () => {
+        fetch("https://springboot-postgresql-heroku.herokuapp.com/api/v1/employees", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(this.state),
+          }).then((response) => {
+            if (response.status === 200) {
+              return response.json().then((res) => {
+                console.log(res)
+      
+              });
+            }
+            else {
+              alert("Wrong Username or password");
+              // console.log(response);
+            }
+          });
+    }
+    render() {
+        return (
+          <div className='body'>
+            <Grid
+              textAlign="center"
+              style={{ height: "100vh" }}
+              verticalAlign="middle"
+            >
+              <Grid.Column style={{ maxWidth: 400 }}>
+                  <Image src={"https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg"} />
+
+                <Form size="large">
+                  <Segment stacked>
+                  <p className='log'>Log in to Facebook</p>
+                 
+                    <Form.Input
+                    className='input'
+                      fluid
+                      placeholder="Email address or phone number"
+                      onChange={this.handleInput("firstName")}
+                      value = {this.state.firstName}
+                      
+                    />
+                    <Form.Input
+                    className='input'
+                      fluid
+                      placeholder="Password"
+                      type="Password"
+                      onChange={this.handleInput("lastName")}
+                      value = {this.state.lastName}
+                      
+                    />
+
+                    <Button
+                      color='blue'
+                      fluid
+                      size="large"
+                      onClick={this.submit}
+                   
+                    >
+                      Log In
+                    </Button>
+                    <br/>
+                    <a>Forgotten account?</a>
+                    <br/>
+                    <Divider horizontal size="small"><p className='orrr'>or</p></Divider>
+                    <br/>
+                    <Button positive>Create New Account</Button>
+                  </Segment>
+                </Form>
+                
+              </Grid.Column>
+            </Grid>
+          </div>
+        );
+    }
 }
 
-export default App;
